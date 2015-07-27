c94d7cd3a9837252df151dab19ff63d1f3cf95a9
BEM <BEM@192.168.2.26>
2015-07-27 11:57:57 +0300

    Date() eklendi

--- src/merhabaprj/MerhabaPrj.java
+++ src/merhabaprj/MerhabaPrj.java
@@ -7,6 +7,7 @@ public class MerhabaPrj {
     
     public static void main(String[] args) {
         System.out.println("Merhaba Dünyalılar....");
+        System.out.println(new java.util.Date());
     }
     
 }
